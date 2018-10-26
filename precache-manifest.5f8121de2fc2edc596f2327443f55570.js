5da5688a93b22a0cdda115fa13d546713625f768
Sebastiaan Schoonebeek <sebastiaan@websnacks.nl>
2018-10-26 13:55:54 +0100

    Updates

---
+++ precache-manifest.5f8121de2fc2edc596f2327443f55570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "159adbcac497513bc366",
+    "url": "/contactmanager/static/js/main.159adbca.chunk.js"
+  },
+  {
+    "revision": "e1f5774e7f32f0c4ce67",
+    "url": "/contactmanager/static/js/1.e1f5774e.chunk.js"
+  },
+  {
+    "revision": "159adbcac497513bc366",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "e1f5774e7f32f0c4ce67",
+    "url": "/contactmanager/static/css/1.e3d1491c.chunk.css"
+  },
+  {
+    "revision": "0bcbf66a6774373a45b5a43a40762a2b",
+    "url": "/contactmanager/index.html"
+  }
+];
